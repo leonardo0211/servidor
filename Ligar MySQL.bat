4f4f046e0c9b19ddfe8f35ae62278333a3c2f563
leonardo0211 <58312663+leonardo0211@users.noreply.github.com>
2020-06-27 00:21:42 -0300

    Add files via upload

---
+++ Ligar MySQL.bat
@@ -0,0 +1,14 @@
+@echo off
+COLOR 0A
+cls
+@echo.
+@echo ****************************************************************************
+@echo **********                    ZdanCore - Oficial                  **********
+@echo **********              Repack Gratis / Free Download             **********
+@echo **********                  www.youtube.com/zdannn                **********
+@echo ****************************************************************************
+
+@echo off
+COLOR 0b
+java -Xmx1G -jar server.jar
+PAUSE
